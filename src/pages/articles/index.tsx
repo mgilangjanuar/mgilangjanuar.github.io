64afdbcc6294eb8798233084b275eba8b1c7e367
M Gilang Januar <mgilangjanuar@gmail.com>
2022-03-02 08:37:09 +0700

    add pagination in articles

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,21 +1,36 @@
-import { Col, Layout, Row, Spin, Typography } from 'antd'
+import { Col, Layout, Pagination, Row, Spin, Typography } from 'antd'
 import axios from 'axios'
 import parseMD from 'parse-md'
 import { FC, useEffect, useState } from 'react'
-import { RAW_URL } from '../../utils/Constant'
+import { PER_PAGE, RAW_URL } from '../../utils/Constant'
 import { Content } from '../article/components/ContentType'
 import CardArticle from './components/CardArticle'
 import Header from './components/Header'
 
 const Articles: FC = () => {
+  const [links, setLinks] = useState<string[]>()
+  const [page, setPage] = useState<number>(1)
   const [pages, setPages] = useState<Content[]>([])
 
   useEffect(() => {
     async function fetchArticles() {
       const { data } = await axios.get(`${RAW_URL}/contents/index.md`)
       const { metadata } = parseMD(data)
+      setLinks((metadata as { articles: string[] }).articles)
+    }
+    fetchArticles()
+  }, [])
+
+  useEffect(() => {
+    fetch()
+  }, [links, page])
+
+  const fetch = async () => {
+    if (links?.length) {
+      setPages([])
       const posts: any[] = []
-      for (const link of (metadata as { articles: string[] }).articles) {
+      const [offset, limit] = [(page - 1) * PER_PAGE, (page - 1) * PER_PAGE + PER_PAGE]
+      for (const link of links.slice(offset, limit)) {
         const { data } = await axios.get(`${RAW_URL}/contents${link}`)
         posts.push({
           link: link.replace(/\.md$/gi, ''),
@@ -24,8 +39,7 @@ const Articles: FC = () => {
       }
       setPages(posts)
     }
-    fetchArticles()
-  }, [])
+  }
 
   return <Layout.Content style={{ marginTop: '60px' }}>
     <Row className="container">
@@ -34,6 +48,17 @@ const Articles: FC = () => {
 
         {!pages?.length && <Typography.Paragraph style={{ textAlign: 'center' }}><Spin /></Typography.Paragraph>}
         {pages?.map(page => <CardArticle key={page.link} page={page} />)}
+
+        <Typography.Paragraph style={{ textAlign: 'right' }}>
+          <Pagination
+            // showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+            total={links?.length}
+            defaultCurrent={page}
+            current={page}
+            defaultPageSize={PER_PAGE}
+            pageSize={PER_PAGE}
+            onChange={page => setPage(page)} />
+        </Typography.Paragraph>
       </Col>
     </Row>
   </Layout.Content>
--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -1,1 +1,3 @@
 export const RAW_URL = 'https://raw.githubusercontent.com/mgilangjanuar/mgilangjanuar.github.io/gh-pages'
+
+export const PER_PAGE = 5
